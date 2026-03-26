029ba3d83674e6a6d9211404ec3d3e08efc811fb
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2026-03-26 11:15:09 +0100

    changed status endpoint url

--- dashboard/urls/generic.py
+++ dashboard/urls/generic.py
@@ -3,5 +3,5 @@ from django.urls import path
 from dashboard.views.status import StatusView
 
 urlpatterns: list = [
-    path("", StatusView.as_view(), name="status"),
+    path("status", StatusView.as_view(), name="status"),
 ]
