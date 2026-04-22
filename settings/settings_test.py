51182c8d0d914a7b463150735cfba3d936593a71
Marc Armenter Hierro <marmenter@cells.es>
2026-04-22 17:16:38 +0200

    Resolve MPM-4725 "/feature"

--- settings/settings_test.py
+++ settings/settings_test.py
@@ -4,7 +4,8 @@ from .settings import *
 
 
 
-FORCE_SCRIPT_NAME: str = "/pacer-dashboard"
+FORCE_SCRIPT_NAME = "/pacer-dashboard"
+SOCKETS_CONTEXT_PATH = "/s"
 STATIC_URL = f"{FORCE_SCRIPT_NAME}/static/"
 LOGIN_URL = f"{FORCE_SCRIPT_NAME}/login/"
 LOGIN_REDIRECT_URL = f"{FORCE_SCRIPT_NAME}/"
