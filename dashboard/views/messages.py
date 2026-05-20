112bb653f6f352d69a67a9a8162a2993b69ca8ec
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2026-05-20 16:17:41 +0200

    filters working

--- dashboard/views/messages.py
+++ dashboard/views/messages.py
@@ -7,21 +7,29 @@ from dashboard.models import Message, GroupProfile
 
 class MessagesView(TemplateView):
     template_name: str = "msg_results.html"
+    max_msg_page: int = 20
 
     def get_context_data(self, **kwargs) -> dict:
+        active_filters: list = self.request.GET.getlist("msg-filters")
+        text_search: str = self.request.GET.get("general-search", "")
+
         context: dict = super().get_context_data(**kwargs)
-        page_number: int = self.request.GET.get("page", 1)
-        msgs_queryset: QuerySet = self.get_queryset()
-        messages = Paginator(msgs_queryset.order_by("-id"), 20)
+        page_number: int = int(self.request.GET.get("page", 1))
+        msgs_queryset: QuerySet = self.get_queryset(msg_type_filters=active_filters, search=text_search)
+        messages = Paginator(msgs_queryset.order_by("-id"), self.max_msg_page)
         context["msg_page_obj"] = messages.get_page(page_number)
-        context["msg_types"] = msgs_queryset.values_list("message_type", flat=True).distinct()
         return context
 
 
 
-    def get_queryset(self) -> QuerySet:
+    def get_queryset(self, msg_type_filters: list = [], search: str = "") -> QuerySet:
         user_filter: Q = GroupProfile.get_user_filters(self.request.user)
-        return Message.objects.filter(user_filter)
+        ret = Message.objects.filter(user_filter)
+        if msg_type_filters:
+            ret = ret.filter(message_type__in=msg_type_filters)
+        if search:
+            ret = ret.filter(object_identifiers__icontains=search)
+        return ret
 
 class TemplateGetById(TemplateView):
     template_name: str = "new_message.html"
