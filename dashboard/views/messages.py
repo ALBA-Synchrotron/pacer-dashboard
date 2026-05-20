ee3ef9c10936586a972a19df0bdeece5e9ea4f0f
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2026-05-20 16:51:59 +0200

    all filters added

--- dashboard/views/messages.py
+++ dashboard/views/messages.py
@@ -12,24 +12,31 @@ class MessagesView(TemplateView):
     def get_context_data(self, **kwargs) -> dict:
         active_filters: list = self.request.GET.getlist("msg-filters")
         text_search: str = self.request.GET.get("general-search", "")
+        errored_only: bool = self.request.GET.get("errored-only", "off") == "on"
+        payload_types: list = self.request.GET.getlist("payload-type")
 
         context: dict = super().get_context_data(**kwargs)
         page_number: int = int(self.request.GET.get("page", 1))
-        msgs_queryset: QuerySet = self.get_queryset(msg_type_filters=active_filters, search=text_search)
+        msgs_queryset: QuerySet = self.get_queryset(msg_type_filters=active_filters, search=text_search,
+                                                    errored_only=errored_only, types=payload_types)
         messages = Paginator(msgs_queryset.order_by("-id"), self.max_msg_page)
         context["msg_page_obj"] = messages.get_page(page_number)
         return context
 
-
-
-    def get_queryset(self, msg_type_filters: list = [], search: str = "") -> QuerySet:
+    def get_queryset(self, msg_type_filters: list = [], search: str = "", errored_only: bool = False,
+                     types: list = []) -> QuerySet:
         user_filter: Q = GroupProfile.get_user_filters(self.request.user)
         ret = Message.objects.filter(user_filter)
         if msg_type_filters:
             ret = ret.filter(message_type__in=msg_type_filters)
         if search:
             ret = ret.filter(object_identifiers__icontains=search)
+        if errored_only:
+            ret = ret.filter(errored=True)
+        if types:
+            ret = ret.filter(payload_format__in=(i.lower() for i in types))
         return ret
+
 
 class TemplateGetById(TemplateView):
     template_name: str = "new_message.html"
