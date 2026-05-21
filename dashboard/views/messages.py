e6d246837d3be2dbb9132931400a1cc8081c4977
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2026-05-21 12:24:06 +0200

    amount of MSGS

--- dashboard/views/messages.py
+++ dashboard/views/messages.py
@@ -4,8 +4,10 @@ from django.views.generic import TemplateView
 
 from dashboard.models import Message, GroupProfile
 
-def get_user_filters(request) -> Q:
+MAX_MSG_PER_PAGE: int = 20
 
+
+def get_user_filters(request) -> Q:
     user_filter: Q = GroupProfile.get_user_filters(request.user)
     message_type_filters: list = request.GET.getlist("msg-filters")
     text_search: str = request.GET.get("general-search", "")
@@ -32,8 +34,11 @@ class MessagesView(TemplateView):
         page_number: int = int(self.request.GET.get("page", 1))
 
         msgs_queryset: QuerySet = self.get_queryset()
-        messages = Paginator(msgs_queryset.order_by("-id"), self.max_msg_page)
+        messages = Paginator(msgs_queryset.order_by("-id"), MAX_MSG_PER_PAGE)
+        context["current_page"] = page_number
         context["msg_page_obj"] = messages.get_page(page_number)
+        context["total_msgs"] = msgs_queryset.count()
+        context["current_msgs"] = min(MAX_MSG_PER_PAGE * page_number, msgs_queryset.count())
         return context
 
     def get_queryset(self) -> QuerySet:
@@ -46,10 +51,19 @@ class TemplateGetById(TemplateView):
 
     def get_context_data(self, **kwargs) -> dict:
         context: dict = super().get_context_data(**kwargs)
+
         msg_id: int = self.kwargs.get("msg_id", None)
+        current_page: int = int(self.request.GET.get("current-page", 1))
+        total_msgs: int = int(self.request.GET.get("total-msgs", 0))
+
+        context["current_msgs"] = min(MAX_MSG_PER_PAGE * current_page, total_msgs)
+        context["total_msgs"] = total_msgs
+
         msg: Message | None = self.get_object(msg_id)
         if msg is not None:
             context["msg"] = msg
+            context["current_msgs"] += 1
+            context["total_msgs"] += 1
         return context
 
     def get_object(self, msg_id: int) -> QuerySet:
@@ -57,4 +71,3 @@ class TemplateGetById(TemplateView):
         user_filters &= Q(id=msg_id)
 
         return Message.objects.filter(user_filters).first()
-
